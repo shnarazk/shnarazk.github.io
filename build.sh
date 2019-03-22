560998c2f9c108706ae00b797de5aa05fa087f7c
shujinarazaki <shujinarazaki@protonmail.com>
2019-03-23 01:11:07 +0900

    build.sh: fix base_url

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.54.0/hugo_0.54.0_Linux-64bit.tar.gz
 tar -xzf hugo_0.54.0_Linux-64bit.tar.gz
-./hugo -b https://shnarazk.now.sh
+./hugo -b https://shnarazk.now.sh/
 
