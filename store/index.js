d4587bce2aa4d207dcb37878b36f4304568c48f8
shujinarazaki <shujinarazaki@protonmail.com>
2019-06-25 07:24:58 +0900

    store db.json under article/.json

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-import db from '~/static/db.json'
+import db from '~/article/.json/db.json'
 
 export const state = () => ({
   articles: db.fileMap,
@@ -15,18 +15,6 @@ export const mutations = {
 function buildTags(arts) {
   const bag = []
   const arr = Object.entries(arts)
-  // for (const e in arr) {
-  /// bag.push(e)
-  // const article = arts[key]
-  // if (article.hasOwnPropety('tags')) {
-  //   article.tags.forEach(t => {
-  //     if (bag[t] === undefined) {
-  //       bag.push(t)
-  //     }
-  //     bag[t].push(article)
-  //   })
-  // }
-  // }
   arr.forEach(val => {
     const art = val[1]
     art.tags.forEach(tag => {
