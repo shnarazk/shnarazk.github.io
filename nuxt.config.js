3580364ac34374bb343566cb02b75f76de705dfa
shujinarazaki <shujinarazaki@protonmail.com>
2019-06-26 09:32:56 +0900

    no need to 'break' for markdownit

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,7 +102,7 @@ export default {
   },
   markdownit: {
     injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
-    breaks: true, // 改行コードを<br>に変換する
+    breaks: false, // 改行コードを<br>に変換する
     html: true, // HTML タグを有効にする
     linkify: true, // URLに似たテキストをリンクに自動変換する
     typography: true, // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
