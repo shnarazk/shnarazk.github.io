8151ca710a6b483c6196e6ec47994be1b5b7972a
shnarazk <shnarazk@users.noreply.github.com>
2020-08-10 08:34:11 +0000

    deploy: c0b36248cdccb3a95a2011de147faa1f85257c71

--- sw.js
+++ sw.js
@@ -338,8 +338,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "99601d1f0a872fcc9587d010e5cbe670"
   },
   {
-    "url": "/_nuxt/pages/_year/_slug/ghg/index.9d942b0.js",
-    "revision": "bba965d3dea36bbc29f8562acb55d442"
+    "url": "/_nuxt/pages/_year/_slug/ghg/index.669ae2a.js",
+    "revision": "cdca8ec6f14eada4752ed05bc95168e5"
   },
   {
     "url": "/_nuxt/pages/_year/_slug/index.0e48e06.js",
@@ -370,8 +370,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "7e7195767ae665a9a460608362c51df4"
   },
   {
-    "url": "/_nuxt/runtime.49a6529.js",
-    "revision": "9fc666931a816f1bf78f39afa240f498"
+    "url": "/_nuxt/runtime.a020bee.js",
+    "revision": "44aae6cc6dd4b3c2c868713a74196271"
   }
 ], {
   "cacheId": "just-a-note",
