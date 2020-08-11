40760112d55ed5cb839d0c067a99368c9b64d097
shnarazk <shnarazk@users.noreply.github.com>
2020-08-11 06:12:18 +0000

    deploy: 75aec311f16f9199b8b4b02531150dde1d8df58a

--- sw.js
+++ sw.js
@@ -334,12 +334,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "dbead3eecea6237ca2be9931748467f0"
   },
   {
-    "url": "/_nuxt/app.b6c9428.js",
-    "revision": "edc40c51c0a659b761b1b335f07f6c37"
+    "url": "/_nuxt/app.74a4d19.js",
+    "revision": "80fd20d699b3047ce10e41b88718d274"
   },
   {
-    "url": "/_nuxt/pages/_year/_slug/ghg/index.c2976bd.js",
-    "revision": "35defb6216fa5de50040a46e07ffc84f"
+    "url": "/_nuxt/pages/_year/_slug/ghg/index.f21ada7.js",
+    "revision": "eac82071643bd5516b1c709031aeaac8"
   },
   {
     "url": "/_nuxt/pages/_year/_slug/index.0e48e06.js",
@@ -370,8 +370,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "7e7195767ae665a9a460608362c51df4"
   },
   {
-    "url": "/_nuxt/runtime.a8eaad3.js",
-    "revision": "bc5ff25b1fab784f0bba311a34604bad"
+    "url": "/_nuxt/runtime.1d137c4.js",
+    "revision": "30d39b0eadb37080a51960d6af3e3c1e"
   }
 ], {
   "cacheId": "just-a-note",
