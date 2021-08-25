37089a4cb60ec33c056edfbefa1c4cfbc7fa26f7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-08-25 10:35:43 +0900

    try to update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?refresh=1' },
       {
         rel: 'stylesheet',
         href:
